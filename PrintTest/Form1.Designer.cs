f06dc859e41286ea697d6b2e6b1e7db51dfe0a30
vicmatmar <victormartin@hotmail.com>
2016-09-09 12:30:43 -0500

    screen scalling

--- PrintTest/Form1.Designer.cs
+++ PrintTest/Form1.Designer.cs
@@ -33,15 +33,21 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonEncode = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.numericUpDownScale = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSize = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownDPI = new System.Windows.Forms.NumericUpDown();
             this.buttonPrint = new System.Windows.Forms.Button();
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton_in = new System.Windows.Forms.RadioButton();
+            this.radioButton_mm = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDPI)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -72,13 +78,13 @@
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.Size = new System.Drawing.Size(255, 253);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // buttonEncode
             // 
-            this.buttonEncode.Location = new System.Drawing.Point(13, 297);
+            this.buttonEncode.Location = new System.Drawing.Point(14, 310);
             this.buttonEncode.Name = "buttonEncode";
             this.buttonEncode.Size = new System.Drawing.Size(75, 23);
             this.buttonEncode.TabIndex = 3;
@@ -94,18 +100,28 @@
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.Location = new System.Drawing.Point(279, 13);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.Size = new System.Drawing.Size(255, 253);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
-            // numericUpDownScale
+            // numericUpDownSize
             // 
-            this.numericUpDownScale.Location = new System.Drawing.Point(134, 300);
-            this.numericUpDownScale.Name = "numericUpDownScale";
-            this.numericUpDownScale.ReadOnly = true;
-            this.numericUpDownScale.Size = new System.Drawing.Size(48, 20);
-            this.numericUpDownScale.TabIndex = 5;
-            this.numericUpDownScale.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDownSize.DecimalPlaces = 2;
+            this.numericUpDownSize.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownSize.Location = new System.Drawing.Point(7, 15);
+            this.numericUpDownSize.Name = "numericUpDownSize";
+            this.numericUpDownSize.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDownSize.TabIndex = 5;
+            this.numericUpDownSize.Value = new decimal(new int[] {
+            55,
+            0,
+            0,
+            65536});
+            this.numericUpDownSize.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDownDPI
             // 
@@ -114,14 +130,14 @@
             0,
             0,
             0});
-            this.numericUpDownDPI.Location = new System.Drawing.Point(201, 300);
+            this.numericUpDownDPI.Location = new System.Drawing.Point(306, 313);
             this.numericUpDownDPI.Maximum = new decimal(new int[] {
-            1200,
+            2400,
             0,
             0,
             0});
             this.numericUpDownDPI.Minimum = new decimal(new int[] {
-            1,
+            50,
             0,
             0,
             0});
@@ -133,10 +149,11 @@
             0,
             0,
             0});
+            this.numericUpDownDPI.ValueChanged += new System.EventHandler(this.numericUpDownDPI_ValueChanged);
             // 
             // buttonPrint
             // 
-            this.buttonPrint.Location = new System.Drawing.Point(292, 296);
+            this.buttonPrint.Location = new System.Drawing.Point(381, 305);
             this.buttonPrint.Name = "buttonPrint";
             this.buttonPrint.Size = new System.Drawing.Size(75, 23);
             this.buttonPrint.TabIndex = 7;
@@ -152,14 +169,70 @@
             // 
             this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Size mm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "dpi";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton_in);
+            this.groupBox1.Controls.Add(this.radioButton_mm);
+            this.groupBox1.Controls.Add(this.numericUpDownSize);
+            this.groupBox1.Location = new System.Drawing.Point(98, 288);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(119, 45);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size";
+            // 
+            // radioButton_in
+            // 
+            this.radioButton_in.AutoSize = true;
+            this.radioButton_in.Location = new System.Drawing.Point(61, 25);
+            this.radioButton_in.Name = "radioButton_in";
+            this.radioButton_in.Size = new System.Drawing.Size(33, 17);
+            this.radioButton_in.TabIndex = 7;
+            this.radioButton_in.Text = "in";
+            this.radioButton_in.UseVisualStyleBackColor = true;
+            this.radioButton_in.CheckedChanged += new System.EventHandler(this.radioButton_size_CheckedChanged);
+            // 
+            // radioButton_mm
+            // 
+            this.radioButton_mm.AutoSize = true;
+            this.radioButton_mm.Checked = true;
+            this.radioButton_mm.Location = new System.Drawing.Point(61, 4);
+            this.radioButton_mm.Name = "radioButton_mm";
+            this.radioButton_mm.Size = new System.Drawing.Size(41, 17);
+            this.radioButton_mm.TabIndex = 6;
+            this.radioButton_mm.TabStop = true;
+            this.radioButton_mm.Text = "mm";
+            this.radioButton_mm.UseVisualStyleBackColor = true;
+            this.radioButton_mm.CheckedChanged += new System.EventHandler(this.radioButton_size_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 342);
+            this.ClientSize = new System.Drawing.Size(583, 345);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonPrint);
             this.Controls.Add(this.numericUpDownDPI);
-            this.Controls.Add(this.numericUpDownScale);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.buttonEncode);
             this.Controls.Add(this.pictureBox1);
@@ -169,9 +242,12 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownScale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDPI)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -182,11 +258,16 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonEncode;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.NumericUpDown numericUpDownScale;
+        private System.Windows.Forms.NumericUpDown numericUpDownSize;
         private System.Windows.Forms.NumericUpDown numericUpDownDPI;
         private System.Windows.Forms.Button buttonPrint;
         private System.Windows.Forms.PrintDialog printDialog1;
         private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton_in;
+        private System.Windows.Forms.RadioButton radioButton_mm;
     }
 }
 
--- PrintTest/Form1.cs
+++ PrintTest/Form1.cs
@@ -22,6 +22,8 @@ using Gma.QrCodeNet.Encoding.Windows.Render;
 
 using ThoughtWorks.QRCode.Codec;
 
+using UnitsNet;
+
 namespace PrintTest
 {
     public partial class Form1 : Form
@@ -35,94 +37,6 @@ namespace PrintTest
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            test2();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            PrintDocument pd = new PrintDocument();
-            pd.PrinterSettings.PrinterName = _printer_name;
-
-
-            pd.PrintPage += pd_PrintPage1;
-            if (pd.PrinterSettings.IsValid)
-            {
-                pd.Print();
-            }
-            else
-            {
-                MessageBox.Show("Printer is invalid.");
-            }
-        }
-
-        void pd_PrintPage1(object sender, PrintPageEventArgs e)
-        {
-            int h = (int)(e.PageSettings.PrintableArea.Height);
-            int w = (int)(e.PageSettings.PrintableArea.Width);
-
-            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.M);
-
-            float side = e.PageSettings.PrintableArea.Width;
-            //side = 100; //=> 1"
-            if (e.PageSettings.PrintableArea.Height < e.PageSettings.PrintableArea.Width)
-                side = e.PageSettings.PrintableArea.Height;
-            float modesize = side / 100 * e.Graphics.DpiX;
-
-            QrCode qrCode = qrEncoder.Encode("0123456789");
-
-
-            ISizeCalculation iSizeCal = new FixedModuleSize(2, QuietZoneModules.Zero);
-            // This works with the Brady on a very small label
-            //ISizeCalculation iSizeCal = new FixedCodeSize( (int)modesize-16, QuietZoneModules.Zero);
-
-            DrawingBrushRenderer dRenderer = new DrawingBrushRenderer(iSizeCal, 
-                System.Windows.Media.Brushes.Black, System.Windows.Media.Brushes.White);
-
-            string test = "test1";
-            if (test == "test1")
-            {
-                MemoryStream mem_stream = new MemoryStream();
-                dRenderer.WriteToStream(qrCode.Matrix, ImageFormatEnum.BMP, mem_stream);
-
-                Bitmap bitmap = new Bitmap(mem_stream);
-                bitmap.SetResolution(e.Graphics.DpiX, e.Graphics.DpiY);
-                //bitmap.SetResolution(10, 10);
-                
-                Graphics graphics = Graphics.FromImage(bitmap);
-                e.Graphics.DrawImage(bitmap, 0, 0);
-            }
-            else
-            {
-                System.Windows.Point dpipoint = new System.Windows.Point(e.Graphics.DpiX, e.Graphics.DpiY);
-                BitmapSource bitmapsource = dRenderer.WriteToBitmapSource(qrCode.Matrix, dpipoint);
-                BitmapFrame bitmapframe = BitmapFrame.Create(bitmapsource);
-
-                BitmapEncoder bitmapencoder = new BmpBitmapEncoder();
-                bitmapencoder.Frames.Add(bitmapframe);
-
-                MemoryStream mem_stream = new MemoryStream();
-                bitmapencoder.Save(mem_stream);
-
-                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(mem_stream);
-                e.Graphics.DrawImage(bitmap, 0, 0);
-            }
-
-        }
-
-        void test2()
-        {
-            QRCodeEncoder encoder = new QRCodeEncoder();
-            encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.NUMERIC;
-            encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
-            encoder.QRCodeBackgroundColor = System.Drawing.Color.Black;
-            encoder.QRCodeForegroundColor = System.Drawing.Color.White;
-            encoder.QRCodeScale = 1;
-
-            Bitmap bitmap = encoder.Encode("1234567890");
         }
 
         void test1(PrintPageEventArgs e)
@@ -184,12 +98,96 @@ namespace PrintTest
                 gRenderer.WriteToStream(qrCode2.Matrix, ImageFormat.Png, stream);
             }
 
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            test2();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = _printer_name;
+
+
+            pd.PrintPage += pd_PrintPage1;
+            if (pd.PrinterSettings.IsValid)
+            {
+                pd.Print();
+            }
+            else
+            {
+                MessageBox.Show("Printer is invalid.");
+            }
+        }
+
+        void pd_PrintPage1(object sender, PrintPageEventArgs e)
+        {
+            int h = (int)(e.PageSettings.PrintableArea.Height);
+            int w = (int)(e.PageSettings.PrintableArea.Width);
+
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.M);
+
+            float side = e.PageSettings.PrintableArea.Width;
+            side = 100; //=> 1"
+            if (e.PageSettings.PrintableArea.Height < e.PageSettings.PrintableArea.Width)
+                side = e.PageSettings.PrintableArea.Height;
+            float modesize = side / 100 * e.Graphics.DpiX;
+
+            QrCode qrCode = qrEncoder.Encode("0123456789");
+
+
+            //ISizeCalculation iSizeCal = new FixedModuleSize(2, QuietZoneModules.Zero);
+            // This works with the Brady on a very small label
+            ISizeCalculation iSizeCal = new FixedCodeSize( (int)modesize-16, QuietZoneModules.Zero);
+
+            DrawingBrushRenderer dRenderer = new DrawingBrushRenderer(iSizeCal, 
+                System.Windows.Media.Brushes.Black, System.Windows.Media.Brushes.White);
+
+            string test = "test1";
+            if (test == "test1")
+            {
+                MemoryStream mem_stream = new MemoryStream();
+                dRenderer.WriteToStream(qrCode.Matrix, ImageFormatEnum.BMP, mem_stream);
+
+                Bitmap bitmap = new Bitmap(mem_stream);
+                bitmap.SetResolution(e.Graphics.DpiX, e.Graphics.DpiY);
+                //Graphics graphics = Graphics.FromImage(bitmap);
+                e.Graphics.DrawImage(bitmap, 0, 0);
+            }
+            else
+            {
+                System.Windows.Point dpipoint = new System.Windows.Point(e.Graphics.DpiX, e.Graphics.DpiY);
+                BitmapSource bitmapsource = dRenderer.WriteToBitmapSource(qrCode.Matrix, dpipoint);
+                BitmapFrame bitmapframe = BitmapFrame.Create(bitmapsource);
+
+                BitmapEncoder bitmapencoder = new BmpBitmapEncoder();
+                bitmapencoder.Frames.Add(bitmapframe);
+
+                MemoryStream mem_stream = new MemoryStream();
+                bitmapencoder.Save(mem_stream);
+
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(mem_stream);
+                e.Graphics.DrawImage(bitmap, 0, 0);
+            }
+
+        }
+
+        void test2()
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.NUMERIC;
+            encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+            encoder.QRCodeBackgroundColor = System.Drawing.Color.Black;
+            encoder.QRCodeForegroundColor = System.Drawing.Color.White;
+            encoder.QRCodeScale = 1;
+
+            Bitmap bitmap = encoder.Encode("1234567890");
         }
 
         private void buttonEncode_Click(object sender, EventArgs e)
         {
-
             encodeToPictureBox();
         }
 
@@ -200,8 +198,11 @@ namespace PrintTest
             QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.M);
             QrCode qrCode = qrEncoder.Encode(data);
 
-            ISizeCalculation iSizeCal = new FixedCodeSize((int)numericUpDownScale.Value, QuietZoneModules.Zero);
-            if (numericUpDownScale.Value == 0)
+            float dpi = (float)numericUpDownDPI.Value;
+            float pdpi = pictureBox1.CreateGraphics().DpiX;
+            float ratio = 1;
+            ISizeCalculation iSizeCal;
+            if (numericUpDownSize.Value == 0)
             {
                 // Full screen
                 int side = pictureBox1.Height;
@@ -212,18 +213,29 @@ namespace PrintTest
             }
             else
             {
+                ratio = pdpi / dpi;
+                double dim = (double)numericUpDownSize.Value;
+                if (radioButton_mm.Checked)
+                {
+                    dim = Length.FromMillimeters(dim).Inches;
+                }
+                
+                int pixels = (int)(dim * dpi);  // number of pixels
+                iSizeCal = new FixedCodeSize(pixels, QuietZoneModules.Zero);
 
             }
-            DrawingBrushRenderer dRenderer = new DrawingBrushRenderer(iSizeCal, System.Windows.Media.Brushes.Black, System.Windows.Media.Brushes.White);
-
+            DrawingBrushRenderer dRenderer = new DrawingBrushRenderer(iSizeCal, 
+                System.Windows.Media.Brushes.Black, System.Windows.Media.Brushes.White);
             MemoryStream mem_stream = new MemoryStream();
             dRenderer.WriteToStream(qrCode.Matrix, ImageFormatEnum.BMP, mem_stream);
             Bitmap bitmap = new Bitmap(mem_stream);
-            float dpi = (float)numericUpDownDPI.Value;
-            bitmap.SetResolution(dpi, dpi);
+            bitmap.SetResolution(dpi,dpi);
+            
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            Bitmap pbitmap = new Bitmap(bitmap, (int)(bitmap.Width * ratio), (int)(bitmap.Height * ratio));
+            this.pictureBox1.Image = pbitmap;
 
-            this.pictureBox1.Image = bitmap;
-
+            
             // A different way to do the same.  Just incase the bitmap.SetResolution function does not work
             //System.Windows.Point dpipoint = new System.Windows.Point(300, 300);
             //BitmapSource bitmapsource = dRenderer.WriteToBitmapSource(qrCode.Matrix, dpipoint);
@@ -247,13 +259,10 @@ namespace PrintTest
             this.pictureBox2.Image = image;
 
         }
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void buttonPrint_Click(object sender, EventArgs e)
         {
+
             printDialog1.Document = printDocument1;
             DialogResult r = printDialog1.ShowDialog();
             if (r == DialogResult.OK)
@@ -267,5 +276,32 @@ namespace PrintTest
         {
             e.Graphics.DrawImage(pictureBox1.Image, 0, 0);  
         }
+
+        private void numericUpDownDPI_ValueChanged(object sender, EventArgs e)
+        {
+            encodeToPictureBox();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            encodeToPictureBox();
+        }
+
+        private void radioButton_size_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_mm.Checked)
+            {
+                numericUpDownSize.Maximum = 100;
+                numericUpDownSize.Increment = 1;
+            }
+            else
+            {
+                numericUpDownSize.Maximum = 4;
+                numericUpDownSize.Increment = new Decimal(0.5);
+            }
+
+        }
+
+
+
     }
 }
